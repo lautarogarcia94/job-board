c543643ec303fc905e04ff143e03669eed93b84a
Lautaro Garcia <lautygcr@gmail.com>
2025-09-10 11:50:49 +0200

    Corrected jobs length to 3

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,7 +2,8 @@ import jobs from '../jobs.json';
 import JobCard from './cards/JobCard';
 
 const JobListings = () => {
-  console.log(jobs);
+  const recentJobs = jobs.slice(0, 3);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -10,8 +11,8 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job, index) => (
-            <JobCard key={index} job={job} />
+          {recentJobs.map((job) => (
+            <JobCard key={job.id} job={job} />
           ))}
         </div>
       </div>
