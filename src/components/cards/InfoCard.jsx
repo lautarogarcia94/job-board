086a7de24ff8d5fb21348cc08fc51b7a7490a5a6
Lautaro Garcia <lautygcr@gmail..com>
2025-09-10 15:23:30 +0200

    Corrected visualization of Job Description in HomePage

--- src/components/cards/InfoCard.jsx
+++ src/components/cards/InfoCard.jsx
@@ -1,7 +1,6 @@
 import Card from './Card';
 
 const InfoCard = ({ data }) => {
-  console.log('informationCard:', data);
   return (
     <Card className="p-6" bg={data.bg}>
       <h2 className="text-2xl font-bold">{data.title}</h2>
