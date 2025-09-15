973e612c55c1f4a4e14d17542f6dc3a3bd4763cc
Lautaro Garcia <lautygcr@gmail..com>
2025-09-15 10:14:28 +0200

    Added delete job functionality

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,16 +1,19 @@
 import { ClipLoader } from 'react-spinners';
 
-const Spinner = ({ loading }) => {
-  const override = {
+const Spinner = ({ loading, size = 150, override: customOverride }) => {
+  const defaultOverride = {
     display: 'block',
     margin: '100px auto',
   };
+
+  const mergedOverride = { ...defaultOverride, ...customOverride };
+
   return (
     <ClipLoader
       color="#4338ca"
       loading={loading}
-      cssOverride={override}
-      size={150}
+      cssOverride={mergedOverride}
+      size={size}
     />
   );
 };
