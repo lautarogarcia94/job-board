de7b464398e15fed23e72c331a2dc308157f45bf
Lautaro Garcia <lautygcr@gmail..com>
2025-09-16 16:11:00 +0200

    Added Scroll to top for all pages

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
+import ScrollToTop from '../components/ScrollToTop';
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-indigo-50">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <ToastContainer />
